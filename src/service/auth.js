b5667ad44e453479f04bb9efc83ea0a3e6dabed9
Guilherme Arruda <guilherme_arruda@live.com>
2019-11-30 03:56:56 -0300

    Alteração rota backend

--- src/service/auth.js
+++ src/service/auth.js
@@ -1,5 +1,5 @@
 export const TOKEN_KEY = "jwt_token";
-export const API_URL = "http://localhost:8080";
+export const API_URL = "https://rds-backend.herokuapp.com";
 
 export const isAuthenticated = () => {
   if (sessionStorage.getItem(TOKEN_KEY) !== null) {
